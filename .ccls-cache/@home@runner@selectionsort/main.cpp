0981d529464d4875bd751abab31af21c8f6249ca
ekas06 <162560857+ekas06@users.noreply.github.com>
2024-03-07 09:37:49 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@selectionsort/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int arr[5]={6,34,4,54,9};
+  for(int i=0;i<4;i++){
+    for(int j=i+1;j<5;j++){
+      if (arr[i]>arr[j]){
+        int temp=arr[i];
+         arr[i]=arr[j];
+        arr[j]=temp;
+      }
+      
+    }
+  }
+  for(int i=0;i<5;i++){
+    cout<<arr[i]<<" ";
+  }
+}
